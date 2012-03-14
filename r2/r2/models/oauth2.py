639fdf1ccef78942fc5dff72f69fe788fd2a8d9b
Max Goodman <max@reddit.com>
2012-03-14 15:16:27 -0700

    Add support for multiple scopes per OAuth2 access token.

--- r2/r2/models/oauth2.py
+++ r2/r2/models/oauth2.py
@@ -213,7 +213,8 @@ class OAuth2AuthorizationCode(OAuth2Token):
     _connection_pool = 'main'
 
     @classmethod
-    def _new(cls, client_id, redirect_uri, user_id, scope):
+    def _new(cls, client_id, redirect_uri, user_id, scope_list):
+        scope = ','.join(scope_list)
         return super(OAuth2AuthorizationCode, cls)._new(
                 client_id=client_id,
                 redirect_uri=redirect_uri,
@@ -250,7 +251,8 @@ class OAuth2AccessToken(OAuth2Token):
     _connection_pool = 'main'
 
     @classmethod
-    def _new(cls, user_id, scope):
+    def _new(cls, user_id, scope_list):
+        scope = ','.join(scope_list)
         return super(OAuth2AccessToken, cls)._new(
                      user_id=user_id,
                      scope=scope)
@@ -323,6 +325,10 @@ class OAuth2AccessToken(OAuth2Token):
                 tokens.append(token)
 
         return tokens
+
+    @property
+    def scope_list(self):
+        return self.scope.split(',')
 
 class OAuth2AccessTokensByUser(tdb_cassandra.View):
     """Index listing the outstanding access tokens for an account."""
